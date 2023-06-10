2749ca084fca4b955b25e09a9304a86d7af6a58b
RaulGB88 <raul.granel@gmail.com>
2023-06-10 15:42:17 +0200

    feat-movimientos v0.0.1.2
    OnItemClickListener

--- app/src/main/java/com/practicas/pmdm/mybank/LogedActivity.java
+++ app/src/main/java/com/practicas/pmdm/mybank/LogedActivity.java
@@ -2,27 +2,23 @@ package com.practicas.pmdm.mybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.telecom.Call;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.practicas.pmdm.mybank.adapter.CuentaArrayAdapter;
 import com.practicas.pmdm.mybank.dao.CuentaDAO;
 import com.practicas.pmdm.mybank.pojo.Cliente;
 import com.practicas.pmdm.mybank.pojo.Cuenta;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
-public class LogedActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class LogedActivity extends AppCompatActivity
+        implements AdapterView.OnItemClickListener {
 
     CuentaDAO cuentaDAO = new CuentaDAO();
     CuentaArrayAdapter cuentaArrayAdapter = null;
@@ -41,39 +37,16 @@ public class LogedActivity extends AppCompatActivity implements AdapterView.OnIt
         // 1. Get list from Dao (Dao List).
         ArrayList<Cuenta> listCuenta = cuentaDAO.getCuentas(cliente);
         // 2. Instance Adapter and put list into it (adapter(list)).
-        cuentaArrayAdapter = new CuentaArrayAdapter(this, listCuenta);
+        cuentaArrayAdapter = new CuentaArrayAdapter(this, 0, listCuenta);
         // 3. Get list component from the listview (findViewById - list).
-        lvMenu = findViewById(R.id.lvList);
+        //lvMenu = findViewById(R.id.lvList);
         // 4. Set Adapter(Rows) into list component (setAdapter).
-        lvMenu.setAdapter(cuentaArrayAdapter);
-        /*
-        lvMenu.setOnClickListener(lvMenu.setOnClickListener(this.lvMenu) {
-            /**
-             * Callback method to be invoked when an item in this AdapterView has
-             * been clicked.
-             * <p>
-             * Implementers can call getItemAtPosition(position) if they need
-             * to access the data associated with the selected item.
-             *
-             * @param parent   The AdapterView where the click happened.
-             * @param view     The view within the AdapterView that was clicked (this
-             *                 will be a view provided by the adapter)
-             * @param position The position of the view in the adapter.
-             * @param id       The row id of the item that was clicked.
-             *
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Cuenta cuentaClicked = listCuenta.get(position);
-                Intent intent = new Intent(LogedActivity.this, MovementActivity.class);
-                intent.putExtra("id", cuentaClicked.getId());
-                intent.putExtra("banco", cuentaClicked.getBanco());
-                intent.putExtra("sucursal", cuentaClicked.getSucursal());
-                intent.putExtra("dc", cuentaClicked.getDc());
-                intent.putExtra("numeroCuenta", cuentaClicked.getNumeroCuenta());
-                intent.putExtra("saldoActual", cuentaClicked.getSaldoActual());
-                startActivity(intent);
-            }
-        }); */
+        //lvMenu.setAdapter(cuentaArrayAdapter);
+        // 5. Set event to onclick.
+        ListView list = (ListView) findViewById(R.id.lvList);
+        list.setAdapter(cuentaArrayAdapter);
+        list.setClickable(true);
+        list.setOnItemClickListener(this);
 
         // 5. Call the view if it's different than the actual view (return View).
         //startActivity(pageReturned);
@@ -108,12 +81,15 @@ public class LogedActivity extends AppCompatActivity implements AdapterView.OnIt
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Cuenta cuentaClicked = cuentaArrayAdapter.getItem(position);
         Intent intent = new Intent(LogedActivity.this, MovementActivity.class);
+        intent.putExtra("cuenta", cuentaClicked);
+        /*
         intent.putExtra("id", cuentaClicked.getId());
         intent.putExtra("banco", cuentaClicked.getBanco());
         intent.putExtra("sucursal", cuentaClicked.getSucursal());
         intent.putExtra("dc", cuentaClicked.getDc());
         intent.putExtra("numeroCuenta", cuentaClicked.getNumeroCuenta());
         intent.putExtra("saldoActual", cuentaClicked.getSaldoActual());
+        */
         startActivity(intent);
     }
 }
--- app/src/main/java/com/practicas/pmdm/mybank/MovementActivity.java
+++ app/src/main/java/com/practicas/pmdm/mybank/MovementActivity.java
@@ -2,16 +2,39 @@ package com.practicas.pmdm.mybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import com.practicas.pmdm.mybank.adapter.MovementArrayAdapter;
+import com.practicas.pmdm.mybank.dao.MovimientoDAO;
+import com.practicas.pmdm.mybank.pojo.Cuenta;
+import com.practicas.pmdm.mybank.pojo.Movimiento;
+
+import java.util.ArrayList;
 
 public class MovementActivity extends AppCompatActivity {
+
+    MovimientoDAO movimientoDAO = new MovimientoDAO();
+    MovementArrayAdapter movementArrayAdapter = null;
+
+    ListView lvMenu = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movement);
+
+        Cuenta cuenta = (Cuenta) getIntent().getSerializableExtra("cuenta");
+
+        // 1. Get list from Dao (Dao List).
+        ArrayList<Movimiento> listMovement = movimientoDAO.getMovimientos(cuenta);
+        // 2. Instance Adapter and put list into it (adapter(list)).
+        movementArrayAdapter = new MovementArrayAdapter(this, 0, listMovement);
+        // 3. Get list component from the listview (findViewById - list).
+        lvMenu = findViewById(R.id.lvList);
+        // 4. Set Adapter(Rows) into list component (setAdapter).
+        lvMenu.setAdapter(movementArrayAdapter);
+        lvMenu.setOnClickListener((View.OnClickListener) this);
     }
 
 }
